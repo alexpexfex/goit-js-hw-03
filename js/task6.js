336a20a03a5d9d3e8215b56768fdcd45809d958a
Alex Bondar <bondaroleksii92@ukr.net>
2021-01-05 10:56:29 +0200

    dz6 done

---
+++ js/task6.js
@@ -0,0 +1,30 @@
+// Напиши функцию calculateTotalPrice(allProdcuts, productName),
+// которая получает массив объектов и имя продукта(значение свойства name).
+// Возвращает общую стоимость продукта(цена * количество).
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = function (allProdcuts, productName) {
+  // твой код
+  let totalProduct = 0;
+  for (const product of allProdcuts) {
+    let name = product.name;
+
+    if (productName === name) {
+      totalProduct += product.price * product.quantity;
+    }
+  }
+  return totalProduct;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
